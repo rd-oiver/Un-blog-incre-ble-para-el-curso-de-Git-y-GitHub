b96c4554bec9ea9984475e833c35836924946796
Oiver Tarrillo <oiver.tarrillo.rockdrill@gmail.com>
2024-04-26 12:33:30 -0500

    Se agregó el link

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
     <title>TÍTULO DEL POST</title>
     <link rel="stylesheet" href="css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-
-    <script src="https://kit.fontawesome.com/74e2056708.js" crossorigin="anonymous"></script>
 </head>
 
 <style>
@@ -22,7 +22,7 @@
 
 <body>
     <div class="container">
-        <div class="mb-4" id="cabecera">BIENVENIDO A BLOG-POST 3.0</div>
+        <div class="mb-4" id="cabecera">BIENVENIDO A BLOG-POST 2.0</div>
         <div class="text-center" id="post">
             <h1>Este es el título atractivo e interesante del post</h1>
             <p>Este es el párrafo de inicio donde vamos a explicar las cosas increíbles que se pueden hacer con ramas.
@@ -77,16 +77,16 @@
                 <div class="row">
                     <div class="col-md-8 col-sm-6 col-xs-12">
                         <p class="copyright-text">Copyright &copy; 2024 Todos los derechos reservados por:
-                            <a href="#">@TI</a>.
+                            <a href="#">Oiver Tarrillo</a>.
                         </p>
                     </div>
 
                     <div class="col-md-4 col-sm-6 col-xs-12">
                         <ul class="social-icons">
-                            <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a class="facebook" href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
+                            <li><a class="twitter" href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+                            <li><a class="dribbble" href="#"><i class="fa fa-dribbble fa-lg"></i></a></li>
+                            <li><a class="linkedin" href="#"><i class="fa fa-linkedin fa-lg"></i></a></li>
                         </ul>
                     </div>
                 </div>
